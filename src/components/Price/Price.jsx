51785cf4b2a28129cf5aaf76095c11ed8e4daaf9
Raushan Kumar <147742112+raushkum4590@users.noreply.github.com>
2024-06-01 23:31:32 +0530

    Price.jsx

--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -1,23 +1,20 @@
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faToggleOff } from '@fortawesome/free-solid-svg-icons';
+
 const Price = () => {
   return (
-    <div className="flex justify-center">
-
-    <div className="flex justify-between rounded-xl items-center  border  w-{600px} h-12 p-4">
-      <div className="flex justify-start p-5 ">
-        <p className="border-r-2 border-gray-100 pr-4 text-base font-medium"> Display total price</p>
-        <p> Include all fees,before taxes</p>
-        <div className='justify-end space-between pl-16'>
-        <FontAwesomeIcon icon={ faToggleOff} /> 
+    <div className="flex justify-center w-full p-4">
+      <div className="flex flex-col md:flex-row justify-between rounded-xl items-center border w-full max-w-md md:max-w-2xl h-auto md:h-12 p-4">
+        <div className="flex flex-col md:flex-row items-start md:items-center md:justify-start md:p-5 w-full md:w-auto">
+          <p className="md:border-r-2 border-gray-100 md:pr-4 text-base font-medium">Display total price</p>
+          <p className="text-sm md:text-base md:pl-4">Include all fees, before taxes</p>
         </div>
-
-
-
+        <div className="flex justify-end w-full md:w-auto mt-4 md:mt-0">
+          <FontAwesomeIcon icon={faToggleOff} />
+        </div>
       </div>
-      </div></div>
-  )
+    </div>
+  );
 }
 
-export default Price
+export default Price;
